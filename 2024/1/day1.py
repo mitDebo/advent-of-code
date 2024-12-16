c8317770a68cca0fd92e136fa83798145e1f8e96
Kelly Weaver <kel.weaver@gmail.com>
2024-12-16 08:12:29 -0500

    day 1 part 1 solved

--- 2024/1/day1.py
+++ 2024/1/day1.py
@@ -1,1 +1,18 @@
-print("This line will be printed.")
+f = open("input.txt")
+
+l1 = []
+l2 = []
+
+for line in f:
+    s = line.split("   ")
+    l1.append(int(s[0]))
+    l2.append(int(s[1]))
+
+l1.sort()
+l2.sort()
+
+total = 0
+for i in range(len(l1)):
+    total += abs(l1[i] - l2[i])
+
+print(total)
