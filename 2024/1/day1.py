d3d6d785828661c1950c4fcedd70b2c19b6ebea4
Kelly Weaver <kel.weaver@gmail.com>
2024-12-16 08:22:12 -0500

    day 1 complete

--- 2024/1/day1.py
+++ 2024/1/day1.py
@@ -2,17 +2,28 @@ f = open("input.txt")
 
 l1 = []
 l2 = []
+occur = {}
 
 for line in f:
     s = line.split("   ")
-    l1.append(int(s[0]))
-    l2.append(int(s[1]))
+    i1 = int(s[0])
+    i2 = int(s[1])
+
+    l1.append(i1)
+    l2.append(i2)
+    if i2 not in occur:
+        occur[i2] = 0
+    occur[i2] += 1
 
 l1.sort()
 l2.sort()
 
-total = 0
+distanceTotal = 0
+dupeTotal = 0
 for i in range(len(l1)):
-    total += abs(l1[i] - l2[i])
+    distanceTotal += abs(l1[i] - l2[i])
+    if l1[i] in occur:
+        dupeTotal += l1[i] * occur[l1[i]]
 
-print(total)
+print("Answer one: " + str(distanceTotal))
+print("Answer two: " + str(dupeTotal))
