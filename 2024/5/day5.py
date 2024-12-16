0eb0cfb0046011d67e1b4fefbdf7f53ace5ced2f
Kelly Weaver <kel.weaver@gmail.com>
2024-12-16 17:34:26 -0500

    day 5 complete

--- 2024/5/day5.py
+++ 2024/5/day5.py
@@ -5,21 +5,35 @@ def add_rule(r:dict, l:str):
     r[value].append(comes_before)
 
 def is_good(page_order:list, r:dict):
-    for i in range(len(pages)):
-        rule = rules.get(pages[i])
+    for i in range(len(page_order)):
+        rule = rules.get(page_order[i])
         # if no rule found, just continue
         if rule is None:
             continue
-        s = set(rule).intersection(set(pages[i:]))
+        s = set(rule).intersection(set(page_order[i:]))
         if len(s) > 0:
             return False
 
     return True
 
+def fix_pages(page_order:list, r:dict) -> list:
+    i = len(page_order) - 1
+    while i >= 0:
+        l = page_order[i:]
+        if not is_good(l, r):
+            temp = page_order[i]
+            page_order[i] = page_order[i + 1]
+            page_order[i + 1] = temp
+            i += 1
+            continue
+
+        i -= 1
+    return page_order
+
 
 rules = {}
 unfixed_total = 0
-total = 0
+fixed_total = 0
 
 with open("input.txt") as f:
     for line in f:
@@ -32,8 +46,9 @@ with open("input.txt") as f:
         pages = line.strip().split(",")
         if is_good(pages, rules):
             unfixed_total += int(pages[len(pages) // 2])
-
-    total += int(pages[len(pages) // 2])
+        else:
+            pages = fix_pages(pages, rules)
+            fixed_total += int(pages[len(pages) // 2])
 
 print("Answer one: {}".format(unfixed_total))
-print("Answer two: {}".format(total))
+print("Answer two: {}".format(fixed_total))
