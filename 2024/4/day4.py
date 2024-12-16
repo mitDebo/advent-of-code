57f87af98328e3c6c6e5c734c9e08d4360c6cf15
Kelly Weaver <kel.weaver@gmail.com>
2024-12-16 12:32:29 -0500

    day 4 answer 2 done

--- 2024/4/day4.py
+++ 2024/4/day4.py
@@ -1,24 +1,43 @@
-def has_value(b:list, x:int, y:int, value:str) -> bool:
+def has_value(b:list, y:int, x:int, value:str) -> bool:
     if y < 0 or y >= len(b) or x < 0 or x >= len(b[y]):
         return False
 
-    return b[x][y] == value
+    return b[y][x] == value
 
-def get_xmas_count(b:list, x:int, y:int) -> int:
-    if b[x][y] != 'X':
+def get_xmas_count(b:list, y:int, x:int) -> int:
+    if b[y][x] != 'X':
         return 0
 
     total = 0
     for vx in [-1, 0, 1]:
         for vy in [-1, 0, 1]:
-            if (has_value(b, x + vx, y + vy, 'M')
-                and has_value(b, x + 2 * vx, y + 2 * vy, 'A')
-                and has_value(b, x + 3 * vx, y + 3 * vy, 'S')):
+            if (has_value(b, y + vy, x + vx, 'M')
+                and has_value(b, y + 2 * vy, x + 2 * vx, 'A')
+                and has_value(b, y + 3 * vy, x + 3 * vx, 'S')):
                 total += 1
 
     return total
 
+def has_double_mas(b:list, y:int, x:int) -> bool:
+    v = b[y][x]
 
+    if v != 'A':
+        return False
+
+    for dx in [-1, 1]:
+        for dy in [-1, 1]:
+            if (has_value(b, y - dy, x + dx, 'M')
+                and has_value(b, y - dy, x - dx, 'M')
+                and has_value(b, y + dy, x - dx, 'S')
+                and has_value(b, y + dy, x + dx, 'S')):
+                return True
+            if (has_value(b, y + dy, x - dx, 'M')
+                and has_value(b, y - dy, x - dx, 'M')
+                and has_value(b, y + dy, x + dx, 'S')
+                and has_value(b, y - dy, x + dx, 'S')):
+                return True
+
+    return False
 
 board = []
 
@@ -30,8 +49,9 @@ total = 0
 double_mas_total = 0
 for y, row in enumerate(board):
     for x, value in enumerate(row):
-        total += get_xmas_count(board, x, y)
-        double_mas_total = get_double_mas_count(board, x, y)
+        total += get_xmas_count(board, y, x)
+        if has_double_mas(board, y, x):
+            double_mas_total += 1
 
 print("Answer one: " + str(total))
 print("Answer two: " + str(double_mas_total))
